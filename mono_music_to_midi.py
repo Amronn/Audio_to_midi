25d3ccdbd81a7a2eb33457a7a7e6bcc7bd385dab
Aaron <zarkadiusz18@gmail.com>
2023-11-08 14:39:12 +0100

    letsgo

---
+++ mono_music_to_midi.py
@@ -0,0 +1,96 @@
+import librosa, librosa.display
+import numpy as np
+from keras.models import load_model
+import sklearn
+from sklearn.preprocessing import MinMaxScaler
+import os
+
+
+model = load_model('monophonic_music_to_midi/notes_88.h5')
+file_path = 'wav_sounds/liszt_frag.wav'
+
+x, sr = librosa.load(file_path)
+
+
+
+def fourier_pitch(segment, sr=sr, fmin=16, fmax=8192):
+    X = np.abs(np.fft.fft(segment))
+    X = X[int(fmin * len(X) / sr):int(fmax * len(X) / sr)]
+    f = np.linspace(fmin, fmax, len(X))
+    note = np.rint(librosa.hz_to_midi(f[np.argmax(X)])).astype(int)
+    return note
+
+def get_folder_names(directory_path):
+    folder_names = []
+    for item in os.listdir(directory_path):
+        item_path = os.path.join(directory_path, item)
+        if os.path.isdir(item_path):
+            folder_names.append(int(item))
+    return folder_names
+
+def get_folder_names2(num):
+    folder_names = []
+    for i in range(num):
+        folder_names.append(i+21)
+    return folder_names
+
+labels = get_folder_names('notes_88')
+# labels = get_folder_names2(88)
+print(labels)
+
+def get_pitch(segment):
+    mel_spec = librosa.feature.melspectrogram(y=segment, sr=sr, hop_length=256, n_mels=128, n_fft=512)
+    scaler = MinMaxScaler()
+    mel_spec = scaler.fit_transform(mel_spec)
+    mel_spec = np.expand_dims(mel_spec, axis=0)
+    prediction = model.predict(mel_spec)
+    pitch_index = np.argmax(prediction)
+    return pitch_index
+
+oenv = librosa.onset.onset_strength(y=x, sr=sr, aggregate=np.mean, detrend=True)
+oenv[oenv < (np.max(oenv) / 10)] = 0
+onset_samples = librosa.onset.onset_detect(y=x, sr=sr, onset_envelope=oenv, backtrack=True, units='samples')
+segment_size = int(sr * 0.05)
+segments = np.array([x[i:i + segment_size] for i in onset_samples])
+
+import matplotlib.pyplot as plt
+
+plt.figure(figsize=(15,6))
+librosa.display.waveshow(x,sr=sr)
+plt.vlines(librosa.samples_to_time(onset_samples), ymin=-1, ymax=1)
+plt.show()
+
+
+# Initialize an empty list to store the pitches for each segment
+pitches_list = []
+
+# Process each segment and collect the pitch in the list
+for segment in segments:
+    pitch = get_pitch(segment)
+    #pitch = fourier_pitch(segment)
+    pitches_list.append(int(labels[pitch]))
+
+
+print(pitches_list)
+
+timesx = librosa.samples_to_time(onset_samples)
+timesx
+times = list(int((timesx[i+1]-timesx[i])*1000) for i in range(len(timesx)-1))
+print(times)
+
+
+
+
+from mido import Message, MidiFile, MidiTrack
+
+mid = MidiFile()
+track = MidiTrack()
+mid.tracks.append(track)
+
+for i, time in enumerate(times):
+    track.append(Message('note_on', note=pitches_list[i], velocity=127, time = 0))
+    track.append(Message('note_off', note=pitches_list[i], velocity=127, time = time))
+
+mid.save('audio_to_midi_test.mid')
+
+
---
+++ training_88.py
@@ -0,0 +1,62 @@
+from keras.models import Sequential
+from keras.layers import Dense, Flatten, Conv2D
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import csv
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+
+
+folder_path = 'notes_88'
+training_data = []
+training_labels = []
+
+for folder_name in os.listdir(folder_path):
+    folder_dir = os.path.join(folder_path, folder_name)
+    if not os.path.isdir(folder_dir):
+        continue
+    note_label = folder_name
+    for filename in os.listdir(folder_dir):
+        if filename.endswith('.csv'):
+            file_path = os.path.join(folder_dir, filename)
+            with open(file_path, 'r') as file:
+                reader = csv.reader(file)
+                mel_spec = list(reader)
+            mel_spec = np.array(mel_spec, dtype=np.float32)
+            training_data.append(mel_spec)
+            training_labels.append(note_label)
+
+print(training_data[0].shape)
+
+# Convert training_data to a NumPy array
+training_data = np.array(training_data)
+
+# Perform label encoding on training_labels
+label_encoder = LabelEncoder()
+training_labels = label_encoder.fit_transform(training_labels)
+print(training_labels)
+X_train, X_test, y_train, y_test = train_test_split(training_data, training_labels, test_size=0.3, random_state=42)
+
+model = Sequential()
+
+model.add(Flatten(input_shape=training_data[0].shape))
+model.add(Dense(640, activation='relu'))
+model.add(Dense(176, activation='relu'))
+model.add(Dense(len(label_encoder.classes_), activation='softmax'))
+
+model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.fit(X_train, y_train, epochs=40, validation_data=(X_test, y_test))
+
+prediction = model.predict(np.expand_dims(X_train[0], axis=0))
+
+predicted_label = np.argmax(prediction)
+
+model.summary()
+
+predicted_note = label_encoder.inverse_transform([predicted_label])[0]
+
+print(f"Predicted Note: {predicted_note}")
+
+model.save('monophonic_music_to_midi/notes_88.h5')
+
